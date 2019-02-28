8d6924b3c42832c4bbddb513dc80694fe5bd5ae1
Sandeep Acharya <acharya877@gmail.com>
2019-02-28 14:36:12 +1100

    add deployable s3 thumbnail generator handler function

--- README.md
+++ README.md
@@ -2,8 +2,29 @@ S3 Thumbnail generator service
 ===
 
 - AWS resources - lambda, S3, IAM
-- docker (for local setup)
+- docker (to containerize python libraries)
 - serverless framework
+
+
+## steps
+
+1. install docker (https://hub.docker.com/search/?type=edition&offering=community)
+
+2. install plugins
+
+    `npm install serverless-python-requirements`
+
+3. update serverless.yml with your own provider.profile, provider.region and custom.bucket values
+
+4. deploy
+
+    `serverless deploy -v`
+
+    deploy update code changes on handler.py
+
+    `sls deploy function -f s3-thumbnail-generator`
+
+5. go to your S3 dash and upload a png file inside the bucket configured in (3)
 
 
 
--- handler.py
+++ handler.py
@@ -1,24 +1,62 @@
-import json
+import boto3
+import cStringIO
+from PIL import Image, ImageOps
+import os
+
+s3 = boto3.client('s3')
+size = int(os.environ['THUMBNAIL_SIZE'])
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def s3_thumbnail_generator(event, context):
+    # parse event
+    print(event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    if (not key.endswith("_thumbnail.png")):
+        # get image
+        image = get_s3_image(bucket, key)
+        # resize the image
+        thumbnail = image_to_thumbnail(image)
+        # get the new filename
+        thumbnail_key = new_filename(key)
+        # upload the file
+        url = upload_to_s3(bucket, thumbnail_key, thumbnail)
+        return url
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
-    return response
+def get_s3_image(bucket, key):
+    response = s3.get_object(Bucket=bucket, Key=key)
+    imagecontent = response['Body'].read()
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    file = cStringIO.StringIO(imagecontent)
+    img = Image.open(file)
+    return img
+
+
+def image_to_thumbnail(image):
+    return ImageOps.fit(image, (size, size), Image.ANTIALIAS)
+
+
+def new_filename(key):
+    key_split = key.rsplit('.', 1)[0]
+    return key_split + "_thumbnail.png"
+
+
+def upload_to_s3(bucket, key, image):
+    # saving the image into a cStringIO object to avoid writing to disk
+    out_thumbnail = cStringIO.StringIO()
+    # file type must be specified
+    image.save(out_thumbnail, 'PNG')
+    out_thumbnail.seek(0)
+
+    response = s3.put_object(
+        ACL='public-read',
+        Body=out_thumbnail,
+        Bucket=bucket,
+        ContentType='image/png',
+        Key=key
+    )
+    print(response)
+
+    url = '{}/{}/{}'.format(s3.meta.endpoint_url, bucket, key)
+    return url
